c4598b2a88b00d2bcb627727401e591f98a41ec9
Samuel Ryberg <samuel.ryberg@gmail.com>
2023-02-26 22:37:03 +0100

    added glad & keycodes, tested config defines

---
+++ Aryl/Aryl/Input/KeyCodes.h
@@ -0,0 +1,141 @@
+#pragma once
+
+// From glfw3.h
+
+// Key
+#define YL_KEY_SPACE              32
+#define YL_KEY_APOSTROPHE         39  /* ' */
+#define YL_KEY_COMMA              44  /* , */
+#define YL_KEY_MINUS              45  /* - */
+#define YL_KEY_PERIOD             46  /* . */
+#define YL_KEY_SLASH              47  /* / */
+#define YL_KEY_0                  48
+#define YL_KEY_1                  49
+#define YL_KEY_2                  50
+#define YL_KEY_3                  51
+#define YL_KEY_4                  52
+#define YL_KEY_5                  53
+#define YL_KEY_6                  54
+#define YL_KEY_7                  55
+#define YL_KEY_8                  56
+#define YL_KEY_9                  57
+#define YL_KEY_SEMICOLON          59  /* ; */
+#define YL_KEY_EQUAL              61  /* = */
+#define YL_KEY_A                  65
+#define YL_KEY_B                  66
+#define YL_KEY_C                  67
+#define YL_KEY_D                  68
+#define YL_KEY_E                  69
+#define YL_KEY_F                  70
+#define YL_KEY_G                  71
+#define YL_KEY_H                  72
+#define YL_KEY_I                  73
+#define YL_KEY_J                  74
+#define YL_KEY_K                  75
+#define YL_KEY_L                  76
+#define YL_KEY_M                  77
+#define YL_KEY_N                  78
+#define YL_KEY_O                  79
+#define YL_KEY_P                  80
+#define YL_KEY_Q                  81
+#define YL_KEY_R                  82
+#define YL_KEY_S                  83
+#define YL_KEY_T                  84
+#define YL_KEY_U                  85
+#define YL_KEY_V                  86
+#define YL_KEY_W                  87
+#define YL_KEY_X                  88
+#define YL_KEY_Y                  89
+#define YL_KEY_Z                  90
+#define YL_KEY_LEFT_BRACKET       91  /* [ */
+#define YL_KEY_BACKSLASH          92  /* \ */
+#define YL_KEY_RIGHT_BRACKET      93  /* ] */
+#define YL_KEY_GRAVE_ACCENT       96  /* ` */
+#define YL_KEY_WORLD_1            161 /* non-US #1 */
+#define YL_KEY_WORLD_2            162 /* non-US #2 */
+
+// Mouse
+#define YL_MOUSE_BUTTON_1         0
+#define YL_MOUSE_BUTTON_2         1
+#define YL_MOUSE_BUTTON_3         2
+#define YL_MOUSE_BUTTON_4         3
+#define YL_MOUSE_BUTTON_5         4
+#define YL_MOUSE_BUTTON_6         5
+#define YL_MOUSE_BUTTON_7         6
+#define YL_MOUSE_BUTTON_8         7
+#define YL_MOUSE_BUTTON_LAST      YL_MOUSE_BUTTON_8
+#define YL_MOUSE_BUTTON_LEFT      YL_MOUSE_BUTTON_1
+#define YL_MOUSE_BUTTON_RIGHT     YL_MOUSE_BUTTON_2
+#define YL_MOUSE_BUTTON_MIDDLE    YL_MOUSE_BUTTON_3
+
+/* Function keys */
+#define YL_KEY_ESCAPE             256
+#define YL_KEY_ENTER              257
+#define YL_KEY_TAB                258
+#define YL_KEY_BACKSPACE          259
+#define YL_KEY_INSERT             260
+#define YL_KEY_DELETE             261
+#define YL_KEY_RIGHT              262
+#define YL_KEY_LEFT               263
+#define YL_KEY_DOWN               264
+#define YL_KEY_UP                 265
+#define YL_KEY_PAGE_UP            266
+#define YL_KEY_PAGE_DOWN          267
+#define YL_KEY_HOME               268
+#define YL_KEY_END                269
+#define YL_KEY_CAPS_LOCK          280
+#define YL_KEY_SCROLL_LOCK        281
+#define YL_KEY_NUM_LOCK           282
+#define YL_KEY_PRINT_SCREEN       283
+#define YL_KEY_PAUSE              284
+#define YL_KEY_F1                 290
+#define YL_KEY_F2                 291
+#define YL_KEY_F3                 292
+#define YL_KEY_F4                 293
+#define YL_KEY_F5                 294
+#define YL_KEY_F6                 295
+#define YL_KEY_F7                 296
+#define YL_KEY_F8                 297
+#define YL_KEY_F9                 298
+#define YL_KEY_F10                299
+#define YL_KEY_F11                300
+#define YL_KEY_F12                301
+#define YL_KEY_F13                302
+#define YL_KEY_F14                303
+#define YL_KEY_F15                304
+#define YL_KEY_F16                305
+#define YL_KEY_F17                306
+#define YL_KEY_F18                307
+#define YL_KEY_F19                308
+#define YL_KEY_F20                309
+#define YL_KEY_F21                310
+#define YL_KEY_F22                311
+#define YL_KEY_F23                312
+#define YL_KEY_F24                313
+#define YL_KEY_F25                314
+#define YL_KEY_KP_0               320
+#define YL_KEY_KP_1               321
+#define YL_KEY_KP_2               322
+#define YL_KEY_KP_3               323
+#define YL_KEY_KP_4               324
+#define YL_KEY_KP_5               325
+#define YL_KEY_KP_6               326
+#define YL_KEY_KP_7               327
+#define YL_KEY_KP_8               328
+#define YL_KEY_KP_9               329
+#define YL_KEY_KP_DECIMAL         330
+#define YL_KEY_KP_DIVIDE          331
+#define YL_KEY_KP_MULTIPLY        332
+#define YL_KEY_KP_SUBTRACT        333
+#define YL_KEY_KP_ADD             334
+#define YL_KEY_KP_ENTER           335
+#define YL_KEY_KP_EQUAL           336
+#define YL_KEY_LEFT_SHIFT         340
+#define YL_KEY_LEFT_CONTROL       341
+#define YL_KEY_LEFT_ALT           342
+#define YL_KEY_LEFT_SUPER         343
+#define YL_KEY_RIGHT_SHIFT        344
+#define YL_KEY_RIGHT_CONTROL      345
+#define YL_KEY_RIGHT_ALT          346
+#define YL_KEY_RIGHT_SUPER        347
+#define YL_KEY_MENU               348
--- Editor/Editor/Editor.cpp
+++ Editor/Editor/Editor.cpp
@@ -1,5 +1,8 @@
 #include "Editor.h"
 
+#include <Aryl/Events/KeyEvent.h>
+#include <Aryl/Input/KeyCodes.h>
+
 Editor::Editor()
 {
 	YL_ASSERT(!myInstance, "Editor already exists!");
@@ -25,4 +28,20 @@ void Editor::OnEvent(Aryl::Event& e)
 {
 	// Print events for debugging
 	YL_CORE_TRACE(e.ToString());
+
+	Aryl::EventDispatcher dispatcher(e);
+
+	dispatcher.Dispatch<Aryl::KeyPressedEvent>([](Aryl::KeyPressedEvent& event)
+		{
+			if (event.GetKeyCode() == YL_KEY_SPACE)
+			{
+#ifdef YL_DEBUG
+				YL_CORE_INFO("Deb: Test Space");
+#elif YL_RELEASE
+				YL_CORE_INFO("Rel: Test Space");
+#endif
+			}
+
+			return false;
+		});
 }
