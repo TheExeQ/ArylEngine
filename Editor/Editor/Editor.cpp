1f7fd9b397981ad0e4b3b5e19d434818b84b5833
Samuel Ryberg <samuel.ryberg@gmail.com>
2023-04-08 20:16:19 +0200

    pinging on macos now works

--- Editor/Editor/Editor.cpp
+++ Editor/Editor/Editor.cpp
@@ -51,6 +51,23 @@ void Editor::OnAttach()
 
 			YL_CORE_TRACE(str + " from {0}:{1}", packet.endpoint.GetAddress().GetAddressString(), packet.endpoint.GetPort());
 			});
+
+		builder = Aryl::UdpSocketBuilder::Create(Aryl::Application::Get().GetNetworkContext());
+		builder->BoundToAddress(Aryl::IPv4Address(address));
+		builder->BoundToPort(0);
+
+		mySender = Aryl::UdpSocketSender::Create(builder->Build());
+
+		std::string data = "Ping";
+
+		Ref<Aryl::NetPacket> packet = CreateRef<Aryl::NetPacket>();
+
+		packet->header.id = Aryl::NetMessageType::StringMessage;
+
+		packet->data.resize(data.size() + 1);
+		std::memcpy(packet->data.data(), data.c_str(), packet->data.size());
+
+		mySender->Send(packet, Aryl::IPv4Endpoint(Aryl::IPv4Address("192.168.1.168"), port));
 	}
 
 	// ENTT REFLECTION TESTING
